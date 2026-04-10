db23e79269d2adbd3afeb37f0f27d9fca9316546
krown <krown62@gmail.com>
2026-04-10 21:47:31 +0300

    sprint 9

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,20 +1,77 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var searchEditText: EditText
+    private lateinit var clearButton: ImageView
+    private lateinit var backButton: ImageView
+
+
+    private var searchText: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        searchEditText = findViewById(R.id.search_edit_text)
+        clearButton = findViewById(R.id.clear_button)
+        backButton = findViewById(R.id.back_button)
+
+        backButton.setOnClickListener { finish() }
+
+        clearButton.setOnClickListener {
+            searchEditText.text.clear()
+            hideKeyboard()
         }
+
+        // TextWatcher
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchText = s.toString()
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        // Фокус и клавиатура
+        searchEditText.requestFocus()
+    }
+
+    // Состояние при повороте экрана
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("SEARCH_TEXT", searchText)
+    }
+
+    // Состояние после поворота
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        searchText = savedInstanceState.getString("SEARCH_TEXT", "")
+
+        if (searchText.isNotEmpty()) {
+            searchEditText.setText(searchText)
+            searchEditText.setSelection(searchText.length)
+            clearButton.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+        searchEditText.clearFocus()
     }
 }
